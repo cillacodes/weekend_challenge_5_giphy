0779112109a5bc34f7c11e2be5d51244934e167d
Priscilla <Priscillayg23@gmail.com>
2017-02-04 16:18:48 -0600

    added readme.md info and changed positioning of elements

---
+++ README.md
@@ -0,0 +1,7 @@
+#Gif Generator
+
+##Author: Priscilla G
+
+**************************
+
+An application that generates gif via a button or search form. Uses Angular, SQL and Express. 
--- server.js
+++ server.js
@@ -2,11 +2,9 @@ var express = require('express');
 var index = require('./routes/index');
 var app = express();
 
-
-
 app.use(express.static('public'));
-app.use('/', index);
 
+app.use('/', index);
 
 var port = process.env.PORT || 3000;
 var server = app.listen(port, function() {
